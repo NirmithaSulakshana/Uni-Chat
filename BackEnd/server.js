4f968bc3ecbbdc76050a15c86f75aed1d351988c
unknown <nairmitha2000@gmail.com>
2024-02-21 01:10:46 +0530

    implement socket.io

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,4 +1,4 @@
-import express  from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -7,9 +7,9 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
-const app = express();
 
 dotenv.config();
 
@@ -18,16 +18,14 @@ dotenv.config();
 //     res.send("Hello Nirmitha");
 // });
 
-
 app.use(express.json()); //to parse the incoming request with JSON paylodes (from req.body)
 app.use(cookieParser());
 
-
 app.use("/api/auth", authRoutes);
-app.use("/api/messages",messageRoutes);
-app.use("/api/users",userRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
-    connectToMongoDB();
-    console.log(`Server Running on port ${PORT}`);
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
 });
